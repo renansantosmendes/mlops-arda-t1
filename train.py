4bfef8a58e7f4445344ef622bcdd6015344a286c
Renan Santos Mendes <renansantosmendes@gmail.com>
2024-02-29 20:12:41 -0300

    changed pipeline folder

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ X_df = pd.DataFrame(X_df, columns=columns_names)
 
 X_train, X_test, y_train, y_test = train_test_split(X_df, y, test_size=0.3, random_state=42)
 
-y_train = y_train -1
+y_train = y_train - 1
 y_test = y_test - 1
 
 y_train.value_counts()
